6d24f86393378c49785793b77c9dfeb81cabb736
Elozino-Lopez <asaijeelozinolopez@gmail.com>
2023-02-24 01:18:49 +0100

    Cleared the comments making blocks of code inactive

--- objects.js
+++ objects.js
@@ -7,46 +7,46 @@ const address = {
     zipcode: 23423
 }
 
-// function showAddress(address) {
-//     for (let key in address) {
-//         console.log(key, ' : ' , address[key]);
-//     }
-// }
+function showAddress(address) {
+    for (let key in address) {
+        console.log(key, ' : ' , address[key]);
+    }
+}
 
-// showAddress(address);
+showAddress(address);
 
 
 
 
 // Exercise 2
-// const address = {
-//     street: '20 Afolabi Street Okota',
-//     city: 'lagos',
-//     zipcode: 23423
-// }
+const address = {
+    street: '20 Afolabi Street Okota',
+    city: 'lagos',
+    zipcode: 23423
+}
 
-// using factory function
-// function showAddress(street, city, zipcode) {
-//     return {
-//         street,
-//         city,
-//         zipcode
-//     }
-// }
+using factory function
+function showAddress(street, city, zipcode) {
+    return {
+        street,
+        city,
+        zipcode
+    }
+}
 
-// const myAddress = showAddress('20 Afolabi Street Okota', 'Lagos',  23423);
-// console.log(myAddress);
+const myAddress = showAddress('20 Afolabi Street Okota', 'Lagos',  23423);
+console.log(myAddress);
 
 
-// using constructor function
-// function ShowAddress (street, city, zipcode) {
-//     this.street = street;
-//     this.city = city;
-//     this.zipcode = zipcode;
-// }
+using constructor function
+function ShowAddress (street, city, zipcode) {
+    this.street = street;
+    this.city = city;
+    this.zipcode = zipcode;
+}
 
-// const myAddress = new ShowAddress('20 Afolabi Street Okota', 'lagos',  23423);
-// console.log(myAddress);
+const myAddress = new ShowAddress('20 Afolabi Street Okota', 'lagos',  23423);
+console.log(myAddress);
 
 
 
@@ -66,58 +66,58 @@ const address2 = new ShowAddress('3 Jubrila Street, Surulere', 'Lagos',  234567)
 
 // Funtion1 - areEqual
 
-// function areEqual(address1, address2) {
-//     const keys1 = Object.keys(address1);
-//     const keys2 = Object.keys(address2);
+function areEqual(address1, address2) {
+    const keys1 = Object.keys(address1);
+    const keys2 = Object.keys(address2);
 
-//     if (keys1.length !== keys2.length) {
-//       return false;
-//     }
-//     for (let key of keys1) {
-//       if (address1[key] !== address2[key]) {
-//         console.log(false);
-//       }
-//       else {
-//         console.log(true);
-//       }
-//     }
-// }
+    if (keys1.length !== keys2.length) {
+      return false;
+    }
+    for (let key of keys1) {
+      if (address1[key] !== address2[key]) {
+        console.log(false);
+      }
+      else {
+        console.log(true);
+      }
+    }
+}
 
-// areEqual(address1, address2);
+areEqual(address1, address2);
 
-//FUnction2 - areSame
+FUnction2 - areSame
 
-// function areSame(address1, address2) {
-//   return ( address1 === address2 ? true : false);
-// }
+function areSame(address1, address2) {
+  return ( address1 === address2 ? true : false);
+}
 
-// console.log(areSame(address1, address2));
+console.log(areSame(address1, address2));
 
 
 
 // EXERCISE4
 
-// const blogPost = {
-//   title: "The Beauty of Flowers",
-//   body: `FLowers come in variety of shapes, colors and sizes.
-//           Their functions vary from improving the quality of
-//           the air around us as well as detoxifying the environment,
-//           to name a few. Examples include: Snake plant, Dunkin, Cudlin, African Bamble.`,
-//   author: "Elozino Lopez",
-//   views: 4000000,
-//   comments : {
+const blogPost = {
+  title: "The Beauty of Flowers",
+  body: `FLowers come in variety of shapes, colors and sizes.
+          Their functions vary from improving the quality of
+          the air around us as well as detoxifying the environment,
+          to name a few. Examples include: Snake plant, Dunkin, Cudlin, African Bamble.`,
+  author: "Elozino Lopez",
+  views: 4000000,
+  comments : {
     
-//     author: "Shapson",
-//     body: "Nice piece. Which flower detoxifies?",
-//     // author: "Faruq",  // Has to be placed in another object
-//     // body: "Bros, leave flower alone abeg, you no be oyinbo! lol..." // Has to be placed in another object
-//   },
-//   isLive: "False"
-// }
+    author: "Shapson",
+    body: "Nice piece. Which flower detoxifies?",
+    // author: "Faruq",  // Has to be placed in another object
+    // body: "Bros, leave flower alone abeg, you no be oyinbo! lol..." // Has to be placed in another object
+  },
+  isLive: "False"
+}
 
-// for (let key in blogPost) {
-//   console.log(blogPost[key]);
-// }
+for (let key in blogPost) {
+  console.log(blogPost[key]);
+}
 
 
 
@@ -144,10 +144,10 @@ delete programming.jokes
 programming.isFun = "True";
 
 // v. Looping Over The Array
-// console.log(Object.keys(programming));
+console.log(Object.keys(programming));
 
 for (let key in programming) {
-  // console.log(programming.languages);
+  console.log(programming.languages);
 }
 
 // vi. To console.log the key
@@ -158,5 +158,5 @@ for (let key in programming) {
 
 // vii. To Console.log all the values
 for (let key in programming) {
-  // console.log(programming[key]);
+  console.log(programming[key]);
 }
